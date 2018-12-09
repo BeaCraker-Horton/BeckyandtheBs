bde9aaff914d2c666272e6f0974cd5229dc80228
Bea Craker-Horton <bea.crakerhorton@icloud.com>
2018-12-09 14:51:33 +0000

    Updates to search page and home page

---
+++ script.js
@@ -0,0 +1,12 @@
+$('.one').click(function() {
+  alert('You have picked medicine')
+})
+$('.two').click(function() {
+  alert('you have picked soup')
+})
+$('.three').click(function() {
+  alert('you have picked tissues')
+})
+$('.four').click(function() {
+  alert('you have picked plasters')
+})
